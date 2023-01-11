beda844358197c1b1ae592f212acba5b1aa0e7bb
Delgado <rxd718@miami.edu>
2023-01-11 15:23:39 -0500

    added eventlisteners (WIP)

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,19 @@
-var saveButton = $("#saveBtn");
 
 var today = dayjs();
 $('#currentDay').text(today.format('MMM D, YYYY'));
-
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(document).ready ($(function () {
+
+  $(".saveBtn").click(function(){
+      console.log("click");
+        var hour = $(this).parent("id").attr;
+        var hourlyTask = $(".description").val();
+        localStorage.setItem(hour,hourlyTask);
+
+  })
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
