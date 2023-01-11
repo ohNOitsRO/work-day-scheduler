f2898f1669322985e3ac2cff60545bf0763d443a
Delgado <rxd718@miami.edu>
2023-01-11 09:49:43 -0500

    added function delay (render all html elements first)

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,7 @@ $('#currentDay').text(today.format('MMM D, YYYY'));
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready ($(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -27,4 +27,4 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 
-});
+}));
