a68d4fea3b5faab368a3ab99a1534f46f79f5320
Delgado <rxd718@miami.edu>
2023-01-12 15:04:52 -0500

    Added timeblock /past/present/future function

--- Develop/script.js
+++ Develop/script.js
@@ -14,13 +14,36 @@ $(document).ready ($(function () {
         localStorage.setItem(hour,hourlyTask);
 
   })
+
+  $("time-block").each(function(){
+      if (hour === dayjs().hour()){
+        $(this).addClass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+      
+      }
+      if (hour < dayjs().hour()){
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+      }
+      if (hour > dayjs().hour()){
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+
+      }
+
+
+  })
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  //    
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
