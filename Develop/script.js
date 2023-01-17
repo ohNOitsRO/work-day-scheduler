a5aeac47ddc4e92c78d827d57775df7ce53cc5e3
Delgado <rxd718@miami.edu>
2023-01-17 10:20:42 -0500

    Fixed Readme document

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,4 @@
+var task = document.querySelector(".description");
 var today = dayjs();
 $('#currentDay').text(today.format('MMM D, YYYY'));
 
@@ -46,8 +47,16 @@ $(document).ready ($(function () {
 
       }
 
-      
-
+    $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
   })
 
